ef0d12bc62e5a6bda8f0e5ff0fc6b6643c9137e4
Raghu Betina <raghu@firstdraft.com>
2017-12-01 21:35:57 +0000

    Whitelist columns in Document for ActiveAdmin

--- app/admin/document.rb
+++ app/admin/document.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Document do
 
+ permit_params :signature_date, :signature, :home_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #

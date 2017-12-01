f087a50eb36560526f373ea472aeaa35136e15de
Raghu Betina <raghu@firstdraft.com>
2017-12-01 21:35:54 +0000

    Whitelist columns in Home for ActiveAdmin

--- app/admin/home.rb
+++ app/admin/home.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Home do
 
+ permit_params :image, :asking_price, :seller_notes, :location, :user_id, :showing_schedule, :bedrooms, :bathrooms, :squarefeet, :acres, :style, :year_built
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
